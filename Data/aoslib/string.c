293ddd87ace43cd7dce04b9875977678da1c2e22
wertikolix <maggisttr33@gmail.com>
2026-03-31 20:14:15 +0300

    fix atoi to use strtoll, add strncmp/memmove/memcmp

--- Data/aoslib/string.c
+++ Data/aoslib/string.c
@@ -38,11 +38,39 @@ void* memcpy(void* dest, const void* src, uint64_t n) {
 }
 
 int32_t strcmp(const char* s1, const char* s2) {
-    while (*s1 && (*s1 == *s2)) { 
-        s1++; 
-        s2++; 
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
     }
     return *(const uint8_t*)s1 - *(const uint8_t*)s2;
+}
+
+int32_t strncmp(const char* s1, const char* s2, uint64_t n) {
+    for (uint64_t i = 0; i < n; i++) {
+        if (s1[i] != s2[i] || s1[i] == '\0')
+            return (uint8_t)s1[i] - (uint8_t)s2[i];
+    }
+    return 0;
+}
+
+void* memmove(void* dest, const void* src, uint64_t n) {
+    uint8_t* d = (uint8_t*)dest;
+    const uint8_t* s = (const uint8_t*)src;
+    if (d < s) {
+        for (uint64_t i = 0; i < n; i++) d[i] = s[i];
+    } else {
+        for (uint64_t i = n; i > 0; i--) d[i-1] = s[i-1];
+    }
+    return dest;
+}
+
+int memcmp(const void* s1, const void* s2, uint64_t n) {
+    const uint8_t* a = (const uint8_t*)s1;
+    const uint8_t* b = (const uint8_t*)s2;
+    for (uint64_t i = 0; i < n; i++) {
+        if (a[i] != b[i]) return a[i] - b[i];
+    }
+    return 0;
 }
 
 char *strcpy(char *dest, const char *src) {
@@ -735,15 +763,15 @@ int kstrtobool(const char *s, bool *res) {
 }
 
 int atoi(const char *str) {
-    return (int)strtoull(str, NULL, 10);
+    return (int)strtoll(str, NULL, 10);
 }
 
 long atol(const char *str) {
-    return (long)strtoull(str, NULL, 10);
+    return (long)strtoll(str, NULL, 10);
 }
 
 long long atoll(const char *str) {
-    return (long long)strtoull(str, NULL, 10);
+    return (long long)strtoll(str, NULL, 10);
 }
 
 static void reverse(char *str, int length) {
