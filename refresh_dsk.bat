cc659e8e4091837d04ec06b882d2bd97b5aef84f
angeloxhek <nikitos.antipov.20@gmail.com>
2026-05-01 22:06:27 +0300

    Delete refresh_dsk.bat

--- refresh_dsk.bat
+++
@@ -1,8 +0,0 @@
-if exist F:\. (
-	copy /y Build\Volume\AOSLDR.BIN F:\
-	xcopy "Build\Volume\DRIVERS" "F:\DRIVERS\" /E /I /H /Y
-	copy /y Build\Volume\tree.elf F:\
-) else (
-	echo F: not found
-)
-pause
